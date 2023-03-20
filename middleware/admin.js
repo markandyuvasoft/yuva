166d9a9ec3bbae63994d850a75ba5081cf6ecb1a
markandyuvasoft <markandyuvasoft408@gmail.com>
2023-03-20 17:52:04 +0530

    correct the admin middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -3,7 +3,13 @@ import Admin from "../models/adminModel.js"
 
 const adminAuth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+      return res.status(400).send({ message: "Authorization header missing" })
+    }
+    
+    const token = authHeader.split(" ")[1]
+    
     const verify = Jwt.verify(token, process.env.JWT_SECRET)
 
     const admin = await Admin.findById(verify._id)
